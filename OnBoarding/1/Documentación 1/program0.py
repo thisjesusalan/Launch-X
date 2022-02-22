6876e66c5afef083e974d2cfc979ac2f65a54f6c
LopezAldaranJesusAlan <jesus13alan@gmail.com>
2022-02-21 22:57:06 -0600

    first commit from ubuntu

---
+++ OnBoarding/1/Documentación 1/program0.py
@@ -0,0 +1,7 @@
+#program.py
+sum = 1 + 2
+print(sum)
+
+# Para correr código de Python en consola
+# 1.- Nos ubicamos en el directorio donde se encuentra el archivo (En la terminal).
+# 2.- Corremos "python3 NombreDelPrograma.py" -> python3 program0.py, en este caso.
